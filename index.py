5294e52ca0f0764fe0c0890133c07d8120addee7
Kevin Chi <kchi3@dxc.com>
2019-07-22 11:26:36 -0500

    refactors player as dictionary, and prevents off -screen movement

--- index.py
+++ index.py
@@ -6,15 +6,30 @@ displayWidth = 500
 displayHeight = 500
 win = pygame.display.set_mode((displayWidth, displayHeight))
 
-pygame.display.set_caption('First Game') 
+pygame.display.set_caption('Pygame') 
 
-x = 50 
-y = 50
-charWidth = 40
-charHeight = 60 
-velocity = 5 
+player = {
+  'x': 50, 
+  'y': 50,
+  'width': 40,
+  'height': 60, 
+  'vel': 20 
+}
 
 running = True 
+
+def preventMovementOffScreen(character): 
+  if character['x'] < 0: 
+    character['x'] = 0 
+  if character['y'] < 0: 
+    character['y'] = 0 
+  if character['x'] > displayWidth - character['width']: 
+    character['x'] = displayWidth - character['width']
+  if character['y'] > displayHeight - character['height']: 
+    character['y'] = displayHeight - character['height']
+
+def drawCharacter(character): 
+  pygame.draw.rect(win, (255, 0, 0), (character['x'], character['y'], character['width'], character['height']))
 
 while running: 
   pygame.time.delay(100) #milliseconds 
@@ -23,19 +38,20 @@ while running:
     if event.type == pygame.QUIT: 
       running = False 
 
-
   keys = pygame.key.get_pressed() 
   if keys[pygame.K_LEFT]: 
-    x -= velocity
-  if keys[pygame.K_RIGHT]: 
-    x += velocity
+    player['x'] -= player['vel']
+  if keys[pygame.K_RIGHT]:
+    player['x'] += player['vel']
   if keys[pygame.K_UP]: 
-    y -= velocity 
+    player['y'] -= player['vel']
   if keys[pygame.K_DOWN]: 
-    y += velocity
+    player['y'] += player['vel']
+
+  preventMovementOffScreen(player)
 
   win.fill((0,0,0)) #makes screen all black before drawing the rectangle again
-  pygame.draw.rect(win, (255, 0, 0), (x, y, charWidth, charHeight))
+  drawCharacter(player)
   pygame.display.update() 
 
 pygame.quit()
